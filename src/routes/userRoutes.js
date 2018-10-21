30db6e08a3de4239b71a7c612237b25233a01535
rodrigo <rodrigomedrano.0108@gmail.com>
2018-10-21 16:53:15 -0400

    make it better

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const customerController = require('../models/user');
 
 router.get('/users', customerController.users);
-router.post('/insertUsers', customerController.insertUsersCliente);
+router.post('/insertUsersCliente', customerController.insertUsersCliente);
 router.post('/ingresar', customerController.ingresar);
 router.post('/ingresarConductor', customerController.ingresarConductor);
 //router.get('/update/:id', customerController.update);
